41f77491e0ec0bb615242b4c32f7e160b8835416
ahyush <ahyush@noreply.codeberg.org>
2025-03-01 23:21:09 -0600

    Club numeric types together (#27)
    All numeric types are identified as 'Numeric' to weed out similar types
    from the GenericTypeScore.

--- clang/tools/clang-tools-extra/cast-chk/PatternDetection.h
+++ clang/tools/clang-tools-extra/cast-chk/PatternDetection.h
@@ -73,13 +73,33 @@ void scoreEdge(CensusKey const &from, CensusKey const &to) {
     auto const logKey = from + " -> " + to;
     CNS_DEBUG_MSG(logKey, "begin");
 
-    auto cleanType = [](CensusKey const &op) -> auto {
+    auto macroType = [](std::string const &type) -> std::string {
+        std::string const numerics[] = {"unsigned", "long", "int", "short", "double", "float"};
+        auto isNumeric = false;
+        for(auto const &ntype: numerics) {
+            if(type.find(ntype) != std::string::npos) {
+                isNumeric = true;
+                break;
+            }
+        }
+        auto isPointer = (type.find("*") != std::string::npos);
+        if(isNumeric & isPointer) {
+            return "Number *";
+        }
+        else if(isNumeric) {
+            return "Number";
+        }
+
+        return type;
+    };
+
+    auto cleanType = [&macroType](CensusKey const &op) -> auto {
         //constexpr auto wordsToRemove = ["const", "const ", "volatile", "volatile "];
         auto op_ = ops(op);
         if(op_.arrayType_) {
-            return op_.arrayType_.value();
+            return macroType(op_.arrayType_.value());
         }
-        return op_.type_;
+        return macroType(op_.type_);
     };
 
 
